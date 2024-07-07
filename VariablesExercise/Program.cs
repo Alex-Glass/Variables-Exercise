bff05f976925fafcfba07191b111daa8643d367b
Alex Glass <alexastro22@gmail.com>
2024-07-06 20:43:40 -0500

    finished exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace VariablesExercise
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿string name = "Alex";
+int age = 29;
+char middleInitial = 'P';
+bool isOver18 = true;
+double currentTemp = 94.2;
+decimal currentPrice = 5.96m;
+
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Middle Initial: {middleInitial}");
+Console.WriteLine($"Is Over 18: {isOver18}");
+Console.WriteLine($"Current Temperature: {currentTemp}");
+Console.WriteLine($"Current Price: {currentPrice}");
+
